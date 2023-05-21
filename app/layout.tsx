c491669929dc58da6d16f3b95504d2bcf4e45d0a
silva4dev <web4dev.lucas@outlook.com>
2023-05-21 11:13:28 -0300

    inicial

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
